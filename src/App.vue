5913431286ca2ced97484b27fc735c27a1f6a911
Phoenix787 <shiny777@bk.ru>
2022-05-13 14:08:40 +0500

    Base concepts

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<div>
+			<button @click="addLike">Like</button>
+			<button @click="addDislike">Dislike</button>
+		</div>
+		<div>Количество лайков: <strong>{{ likes }}</strong></div>
+		<div>Количество дизлайков: <strong>{{ dislikes }}</strong></div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			likes: 0,
+			dislikes: 0,
+		}
+	},
+	methods: {
+		addLike() {
+			return this.likes += 1;
+		},
+		addDislike () {
+			return this.dislikes += 1;
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
+
